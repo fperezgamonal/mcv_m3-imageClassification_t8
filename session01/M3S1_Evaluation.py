241a4129f7de34c9ee8edce8455ab788c4fa87b5
AnaCaballero89 <32468326+AnaCaballero89@users.noreply.github.com>
2017-12-14 23:39:05 +0100

    Add files via upload
    errors fixed

--- session01/M3S1_Evaluation.py
+++ session01/M3S1_Evaluation.py
@@ -2,6 +2,7 @@ import numpy as np
 import cPickle
 from operator import truediv
 
+
 # per a Conf Matrix
 from sklearn.metrics import confusion_matrix
 
@@ -10,7 +11,7 @@ from sklearn.model_selection import KFold
 	
 class M3S1_Evaluation:
 
-	__slots__=['__TP','__TN','__FP','__FN', '__test_labels', 'predictedclass']
+	__slots__=['__TP','__TN','__FP','__FN', '__test_labels', '__predictedclass']
 
 	#initialize vars
 	def __init__(self, TP, TN, FP, FN, test_labels, predictedclass):
@@ -55,12 +56,28 @@ class M3S1_Evaluation:
 		FN = 0;
 		FP = 0;
 		sz= np.size(cm[0])
+		print  'size = ' + str(sz)
 		for i in range(sz):
-			TP += cm[i, i] 
-			FP += np.sum(cm, axis=0)[i] - cm[i, i]  #The corresponding column for class_i - TP
-			FN += np.sum(cm, axis=1)[i] - cm[i, i] # The corresponding row for class_i - TP
-			TN += np.size(cm) - TP -FP -TN
+			# True Positives
+			TP = cm[i, i]
 			
+			# False Negatives
+			fn_mask = np.zeros(cm.shape)
+			fn_mask[i, :] = 1
+			fn_mask[i, i] = 0
+			FN = np.sum(np.multiply(cm, fn_mask))
+			
+			# False Positives
+			fp_mask = np.zeros(cm.shape)
+			fp_mask[:, i] = 1
+			fp_mask[i, i] = 0
+			FP = np.sum(np.multiply(cm, fp_mask))
+
+			# True Negatives
+			tn_mask = 1 - (fn_mask + fp_mask)
+			tn_mask[i, i] = 0
+			TN1 = np.sum(np.multiply(cm, tn_mask))
+		
 		self.__TP = TP
 		self.__FP = FP
 		self.__TN = TN
@@ -69,37 +86,62 @@ class M3S1_Evaluation:
 		return cm
 		
 
-	# KFold crossValidation
-	
+	# KFold crossValidation	
 	def crossValidation (self):
 	
 		# data is an array with our already pre-processed dataset examples
-		
 		kf = KFold(n_splits=8)
 		sum = 0
+		
 		data = zip(self.__test_labels,self.__predictedclass)
-		from nltk.classify import NaiveBayesClassifier
+		#from nltk.classify import NaiveBayesClassifier
 
 		for train, test in kf.split(data):
 			train_data = np.array(data)[train]
+			
+			aux = map(list, (zip(*train_data)))
+			
+			
 			test_data = np.array(data)[test]
+			
 			#classifier = NaiveBayesClassifier.train(train_data)
-			classifier = M3S1_Main.trainClassifier(train_data)
+		
+			# aux[0] => labels
+			# aux[1] =>  predictedclass
+			
+			#parameters of train classifier KNN: test_images_filenames[i], detector, classifier
+			classifier = self.trainClassifier(aux[0], aux[1])
+			
 			#sum += nltk.classify.accuracy(classifier, test_data)
 			
 			# recalculate TP TN FP FN (the sane as  in cross validation)
-			cm= confMatrix(self.__test_labels, self.__predictedclass)	
 			TP = 0;
 			TN = 0;
 			FN = 0;
 			FP = 0;
 			sz= np.size(cm[0])
+			print  'size = ' + str(sz)
 			for i in range(sz):
-				TP += cm[i, i] 
-				FP += np.sum(cm, axis=0)[i] - cm[i, i]  
-				FN += np.sum(cm, axis=1)[i] - cm[i, i] 
-				TN += np.size(cm) - TP -FP -TN
-
+				# True Positives
+				TP = cm[i, i]
+				
+				# False Negatives
+				fn_mask = np.zeros(cm.shape)
+				fn_mask[i, :] = 1
+				fn_mask[i, i] = 0
+				FN = np.sum(np.multiply(cm, fn_mask))
+				
+				# False Positives
+				fp_mask = np.zeros(cm.shape)
+				fp_mask[:, i] = 1
+				fp_mask[i, i] = 0
+				FP = np.sum(np.multiply(cm, fp_mask))
+				
+				# True Negatives
+				tn_mask = 1 - (fn_mask + fp_mask)
+				tn_mask[i, i] = 0
+				TN1 = np.sum(np.multiply(cm, tn_mask))
+			
 			a = TP+TN
 			b = TP+TN+FP+FN
 			sum +=  a/b
@@ -108,27 +150,28 @@ class M3S1_Evaluation:
 			
 		return average 
 		
-	# ROC Curves
-	def rocCurves():
-		return 0
-
 	# print all evaluation data
 	def printEvaluation(self):
-		print "confusion matrix " 
+		print "confusion matrix = " 
 		print self.confMatrix()
 		
-		print "accuracy " +  str(self.accuracy())
-		print "precision " + str(self.precision())
-		print "recall " + str(self.recall())
-		print "f1 Score " + str(self.f1Score())
-		#print "cross validation " + str(self.crossValidation())
+		print "accuracy = " +  str(self.accuracy())
+		print "precision = " + str(self.precision())
+		print "recall = " + str(self.recall())
+		print "f1 Score = " + str(self.f1Score())
+		print "cross validation " + str(self.crossValidation())
 		
-		print "TP" + str(self.__TP)
-		print "TN" + str(self.__TN)
-		print "FP" + str(self.__FP)
-		print "FN" + str(self.__FN)
-		#print "roc curves " + rocCurves()
+		print "TP = " + str(self.__TP)
+		print "TN = " + str(self.__TN)
+		print "FP = " + str(self.__FP)
+		print "FN = " + str(self.__FN)
 	
-	
-	
+	def trainClassifier(D, L):
+		# Train a k-nn classifier
+		
+		print 'Training the knn classifier...'
+		myknn = KNeighborsClassifier(n_neighbors=5,n_jobs=-1)
+		myknn.fit(D,L)
+		print 'Done!'
 
+		return myknn
