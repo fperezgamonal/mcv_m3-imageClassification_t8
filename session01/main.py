a4b8a366e4bf7cd1c7e3906d2aebbd4dde047ba4
cesc <francesc.serracant@gmail.com>
2017-12-18 00:22:17 +0100

    cross_val.py

--- session01/main.py
+++ session01/main.py
@@ -2,15 +2,36 @@
 import time
 from M3S1_Pipeline import M3S1_Pipeline
 
-
-# SIFT with 100 features + 5-KNN
-evaluations = []
-
-
 def testSIFT_KNN():
 	pipeline = M3S1_Pipeline()
 	pipeline.getFeatureExtractor().configureSIFT(60)
 	pipeline.getClassifier().configureKNN(8,-1)
+
+	start = time.time()
+	pipeline.run()
+	end=time.time()
+
+	print 'Done in '+str(end-start)+' secs.'
+
+	pipeline.getEvaluation().printEvaluation()
+
+def testSIFT_Bayes():
+	pipeline = M3S1_Pipeline()
+	pipeline.getFeatureExtractor().configureSIFT(60)
+	pipeline.getClassifier().configureGaussianNBayes()
+
+	start = time.time()
+	pipeline.run()
+	end=time.time()
+
+	print 'Done in '+str(end-start)+' secs.'
+
+	pipeline.getEvaluation().printEvaluation()
+
+def testSIFT_RandomForest():
+	pipeline = M3S1_Pipeline()
+	pipeline.getFeatureExtractor().configureSIFT(60)
+	pipeline.getClassifier().configureRandomForest()
 
 	start = time.time()
 	pipeline.run()
@@ -46,10 +67,36 @@ def testHOG_Bayes():
 
 	pipeline.getEvaluation().printEvaluation()
 	
+def testHOG_RandomForest():
+	pipeline = M3S1_Pipeline()
+	pipeline.getFeatureExtractor().configureHOG(9)
+	pipeline.getClassifier().configureRandomForest()
+
+	start = time.time()
+	pipeline.run()
+	end=time.time()
+
+	print 'Done in '+str(end-start)+' secs.'
+
+	pipeline.getEvaluation().printEvaluation()
+	
+def testHUE_KNN():
+	pipeline = M3S1_Pipeline()
+	pipeline.getFeatureExtractor().configureHueHistogram(100)
+	pipeline.getClassifier().configureKNN(8,-1)
+
+	start = time.time()
+	pipeline.run()
+	end=time.time()
+
+	print 'Done in '+str(end-start)+' secs.'
+
+	pipeline.getEvaluation().printEvaluation()
+	
 def testHUE_Bayes():
 	pipeline = M3S1_Pipeline()
 	pipeline.getFeatureExtractor().configureHueHistogram(100)
-	pipeline.getClassifier().configureGaussianNBayes()
+	pipeline.getClassifier().configureMultinomialNBayes()
 
 	start = time.time()
 	pipeline.run()
@@ -76,4 +123,4 @@ def testHUE_RForest():
 #pipeline.getClassifier().configureGaussianNBayes()
 
 
-testSIFT_KNN()
+testHUE_Bayes()
