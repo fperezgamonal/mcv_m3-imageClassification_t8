1dfcac312905bdd4b03fa5fd6e4642e6c2a44dc5
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2017-12-18 00:20:14 +0100

    SVM changes for next week and new CV functions (HOG)

--- session01/main.py
+++ session01/main.py
@@ -71,6 +71,59 @@ def testHUE_RForest():
 	print 'Done in '+str(end-start)+' secs.'
 
 	pipeline.getEvaluation().printEvaluation()
+
+def testHOG_RForest():
+	pipeline = M3S1_Pipeline()
+	pipeline.getFeatureExtractor().configureHOG(1)
+	pipeline.getClassifier().configureRandomForest()
+
+	start = time.time()
+	pipeline.run()
+	end=time.time()
+
+	print 'Done in '+str(end-start)+' secs.'
+
+	pipeline.getEvaluation().printEvaluation()
+
+def testSIFT_SVM():
+	pipeline = M3S1_Pipeline()
+	pipeline.getFeatureExtractor().configureSIFT(100)
+	pipeline.getClassifier().configureSVM(12.5, 0.5065, 1)
+
+	start = time.time()
+	pipeline.run()
+	end=time.time()
+
+	print 'Done in '+str(end-start)+' secs.'
+
+	pipeline.getEvaluation().printEvaluation()
+
+def testHUE_SVM():
+	pipeline = M3S1_Pipeline()
+	pipeline.getFeatureExtractor().configureHueHistogram(100)
+	pipeline.getClassifier().configureSVM(12.5, 0.5065, 1)
+
+	start = time.time()
+	pipeline.run()
+	end=time.time()
+
+	print 'Done in '+str(end-start)+' secs.'
+
+	pipeline.getEvaluation().printEvaluation()
+
+def testHOG_SVM():
+	pipeline = M3S1_Pipeline()
+	pipeline.getFeatureExtractor().configureHOG(9)
+	pipeline.getClassifier().configureSVM(12.5, 0.5065, 1)
+
+	start = time.time()
+	pipeline.run()
+	end=time.time()
+
+	print 'Done in '+str(end-start)+' secs.'
+
+	pipeline.getEvaluation().printEvaluation()
+
 #pipeline.getFeatureExtractor().configureHueHistogram(100)
 #pipeline.getClassifier().configureKNN(5,-1)
 #pipeline.getClassifier().configureGaussianNBayes()
